d086fd3680ff18e2d3f6b263fd156eef8d2268c7
Ray Davis <raydavis@berkeley.edu>
2019-07-01 16:06:35 -0700

    CLC-7065 Junction can live without the legacy Oracle DB

--- spec/controllers/ping_controller_spec.rb
+++ spec/controllers/ping_controller_spec.rb
@@ -3,7 +3,6 @@ describe PingController do
   context 'User database unavailable' do
     before do
       User::Data.stub(:database_alive?).and_return(false)
-      CampusOracle::Queries.stub(:database_alive?).and_return(true)
     end
     it 'raises error' do
       expect(Rails.logger).to receive(:fatal)
@@ -12,35 +11,10 @@ describe PingController do
     end
   end
 
-  context 'Campus database unavailable' do
-    before do
-      User::Data.stub(:database_alive?).and_return(true)
-      CampusOracle::Queries.stub(:database_alive?).and_return(false)
-    end
-    it 'raises error' do
-      expect(Rails.logger).to receive(:fatal)
-      get :do
-      expect(response.status).to eq 503
-    end
-  end
-
-  context 'Both databases unavailable' do
-    before do
-      User::Data.stub(:database_alive?).and_return(false)
-      CampusOracle::Queries.stub(:database_alive?).and_return(false)
-    end
-    it 'raises error' do
-      expect(Rails.logger).to receive(:fatal)
-      get :do
-      expect(response.status).to eq 503
-    end
-  end
-
-  context 'Both databases available' do
+  context 'User database available' do
     let (:expected) { { 'server_alive' => true }.to_json }
     before do
       User::Data.stub(:database_alive?).and_return(true)
-      CampusOracle::Queries.stub(:database_alive?).and_return(true)
     end
 
     it 'renders a json file with server status' do
