26f32ce2d4700337231d74bc02304d6edc7ac82e
Ray Davis <raydavis@berkeley.edu>
2019-07-16 15:45:07 -0700

    CLC-6677 Remove obvious non-Junction Angular factories

--- src/assets/javascripts/angular/controllers/pages/splashController.js
+++ src/assets/javascripts/angular/controllers/pages/splashController.js
@@ -3,16 +3,6 @@
 /**
  * Splash controller
  */
-angular.module('calcentral.controllers').controller('SplashController', function(apiService, serviceAlertsFactory, $filter, $scope) {
+angular.module('calcentral.controllers').controller('SplashController', function(apiService, $filter, $scope) {
   apiService.util.setTitle('Home');
-
-  serviceAlertsFactory.getAlerts().then(
-    function successCallback(response) {
-      if (response.data && response.data.alert && response.data.alert.title) {
-        $scope.splashNote = response.data.alert;
-      } else {
-        $scope.splashNote = response.data.releaseNote;
-      }
-    }
-  );
 });
