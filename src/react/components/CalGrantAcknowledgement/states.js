d30c361a8fb248bc2e927228ff9fdf57283d5ff9
Ray Davis <raydavis@berkeley.edu>
2019-07-16 16:08:56 -0700

    CLC-6677 Remove React components

--- src/react/components/CalGrantAcknowledgement/states.js
+++
@@ -1,5 +0,0 @@
-const CAL_GRANT_STATE_COMPLETE = 'Complete';
-const CAL_GRANT_STATE_INCOMPLETE = 'Incomplete';
-
-export const isComplete = (acknowledgement) => acknowledgement.status === CAL_GRANT_STATE_COMPLETE;
-export const isIncomplete = (acknowledgement) => acknowledgement.status === CAL_GRANT_STATE_INCOMPLETE;
--- src/react/helpers/stateHelper.js
+++
@@ -1,6 +0,0 @@
-import update from 'immutability-helper';
-
-export function updateStateProperty(component, stateChanges) {
-  const newState = update(component.state, stateChanges);
-  component.setState(newState);
-}
