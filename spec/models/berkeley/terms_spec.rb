3fe1e844e490342e5892e5da9b13c42daacc7f28
Ray Davis <raydavis@berkeley.edu>
2019-09-16 16:40:43 -0700

    CLC-7090 Stop storing months as minutes in H2; add more terms

--- spec/models/berkeley/terms_spec.rb
+++ spec/models/berkeley/terms_spec.rb
@@ -3,155 +3,6 @@ describe Berkeley::Terms do
   before { allow(Settings.hub_term_proxy).to receive(:fake).and_return true }
   subject {Berkeley::Terms.fetch(options)}
 
-  let(:term_data) do
-    [
-      {
-        'career_code'=>'UGRD',
-        'term_id'=>'2198',
-        'term_type'=>'Fall',
-        'term_year'=>'2019',
-        'term_code'=>'D',
-        'term_descr'=>'Fall 2019',
-        'term_begin_date'=>'2019-08-21T00:00:00.000Z',
-        'term_end_date'=>'2019-12-20T00:00:00.000Z',
-        'class_begin_date'=>nil,
-        'class_end_date'=>nil,
-        'instruction_end_date'=>nil,
-        'grades_entered_date'=>nil,
-        'end_drop_add_date'=>'2019-09-18T00:00:00.000Z',
-        'is_summer'=>'N'
-      },
-      {
-        'career_code'=>'UGRD',
-        'term_id'=>'2195',
-        'term_type'=>'Summer',
-        'term_year'=>'2019',
-        'term_code'=>'C',
-        'term_descr'=>'Summer 2019',
-        'term_begin_date'=>'2019-05-28T00:00:00.000Z',
-        'term_end_date'=>'2019-08-16T00:00:00.000Z',
-        'class_begin_date'=>nil,
-        'class_end_date'=>nil,
-        'instruction_end_date'=>nil,
-        'grades_entered_date'=>nil,
-        'end_drop_add_date'=>nil,
-        'is_summer'=>'Y'
-      },
-      {
-        'career_code'=>'UGRD',
-        'term_id'=>'2192',
-        'term_type'=>'Spring',
-        'term_year'=>'2019',
-        'term_code'=>'B',
-        'term_descr'=>'Spring 2019',
-        'term_begin_date'=>'2019-01-15T00:00:00.000Z',
-        'term_end_date'=>'2019-05-17T00:00:00.000Z',
-        'class_begin_date'=>'2019-01-22T00:00:00.000Z',
-        'class_end_date'=>'2019-05-03T00:00:00.000Z',
-        'instruction_end_date'=>'2019-05-10T00:00:00.000Z',
-        'grades_entered_date'=>nil,
-        'end_drop_add_date'=>'2019-02-13T00:00:00.000Z',
-        'is_summer'=>'N'
-      },
-      {
-        'career_code'=>'UGRD',
-        'term_id'=>'2188',
-        'term_type'=>'Fall',
-        'term_year'=>'2018',
-        'term_code'=>'D',
-        'term_descr'=>'Fall 2018',
-        'term_begin_date'=>'2018-08-15T00:00:00.000Z',
-        'term_end_date'=>'2018-12-14T00:00:00.000Z',
-        'class_begin_date'=>'2018-08-22T00:00:00.000Z',
-        'class_end_date'=>'2018-11-30T00:00:00.000Z',
-        'instruction_end_date'=>'2018-12-07T00:00:00.000Z',
-        'grades_entered_date'=>'2018-12-24T00:00:00.000Z',
-        'end_drop_add_date'=>'2018-09-12T00:00:00.000Z',
-        'is_summer'=>'N'
-      },
-      {
-        'career_code'=>'UGRD',
-        'term_id'=>'2185',
-        'term_type'=>'Summer',
-        'term_year'=>'2018',
-        'term_code'=>'C',
-        'term_descr'=>'Summer 2018',
-        'term_begin_date'=>'2018-05-21T00:00:00.000Z',
-        'term_end_date'=>'2018-08-10T00:00:00.000Z',
-        'class_begin_date'=>nil,
-        'class_end_date'=>nil,
-        'instruction_end_date'=>nil,
-        'grades_entered_date'=>nil,
-        'end_drop_add_date'=>nil,
-        'is_summer'=>'Y'
-      },
-      {
-        'career_code'=>'UGRD',
-        'term_id'=>'2182',
-        'term_type'=>'Spring',
-        'term_year'=>'2018',
-        'term_code'=>'B',
-        'term_descr'=>'Spring 2018',
-        'term_begin_date'=>'2018-01-09T00:00:00.000Z',
-        'term_end_date'=>'2018-05-11T00:00:00.000Z',
-        'class_begin_date'=>'2018-01-16T00:00:00.000Z',
-        'class_end_date'=>'2018-04-27T00:00:00.000Z',
-        'instruction_end_date'=>'2018-05-04T00:00:00.000Z',
-        'grades_entered_date'=>'2018-06-30T00:00:00.000Z',
-        'end_drop_add_date'=>'2018-02-16T00:00:00.000Z',
-        'is_summer'=>'N'
-      },
-      {
-        'career_code'=>'UGRD',
-        'term_id'=>'2178',
-        'term_type'=>'Fall',
-        'term_year'=>'2017',
-        'term_code'=>'D',
-        'term_descr'=>'Fall 2017',
-        'term_begin_date'=>'2017-08-16T00:00:00.000Z',
-        'term_end_date'=>'2017-12-15T00:00:00.000Z',
-        'class_begin_date'=>'2017-08-23T00:00:00.000Z',
-        'class_end_date'=>'2017-12-01T00:00:00.000Z',
-        'instruction_end_date'=>'2017-12-08T00:00:00.000Z',
-        'grades_entered_date'=>'2017-12-20T00:00:00.000Z',
-        'end_drop_add_date'=>'2017-09-22T00:00:00.000Z',
-        'is_summer'=>'N'
-      },
-      {
-        'career_code'=>'UGRD',
-        'term_id'=>'2175',
-        'term_type'=>'Summer',
-        'term_year'=>'2017',
-        'term_code'=>'C',
-        'term_descr'=>'Summer 2017',
-        'term_begin_date'=>'2017-05-22T00:00:00.000Z',
-        'term_end_date'=>'2017-08-11T00:00:00.000Z',
-        'class_begin_date'=>nil,
-        'class_end_date'=>nil,
-        'instruction_end_date'=>nil,
-        'grades_entered_date'=>nil,
-        'end_drop_add_date'=>nil,
-        'is_summer'=>'Y'
-      },
-      {
-        'career_code'=>'UGRD',
-        'term_id'=>'2172',
-        'term_type'=>'Spring',
-        'term_year'=>'2017',
-        'term_code'=>'B',
-        'term_descr'=>'Spring 2017',
-        'term_begin_date'=>'2017-01-10T00:00:00.000Z',
-        'term_end_date'=>'2017-05-12T00:00:00.000Z',
-        'class_begin_date'=>nil,
-        'class_end_date'=>nil,
-        'instruction_end_date'=>nil,
-        'grades_entered_date'=>nil,
-        'end_drop_add_date'=>nil,
-        'is_summer'=>'N'
-      }
-    ]
-  end
-
   shared_examples 'a list of campus terms' do
     its(:campus) {should be_is_a Hash}
     it 'is in reverse chronological order' do
@@ -312,12 +163,11 @@ describe Berkeley::Terms do
       before do
         allow(Settings.features).to receive(:hub_term_api).and_return false
         allow(Settings.features).to receive(:use_term_definitions_json_file).and_return false
-        allow(EdoOracle::Queries).to receive(:get_undergrad_terms).with('2162').and_return term_data
       end
       subject { Berkeley::Terms.new(fake_now: DateTime.parse('2018-07-12'), oldest: 'spring-2016') }
       it 'loads terms from edodb with only 2 future terms and sorted in proper order' do
         terms = subject.load_terms_from_edo_db
-        expect(terms.length).to eq 7
+        expect(terms.length).to eq 8
         expect(terms[0].to_english).to eq 'Spring 2019'
         expect(terms[1].to_english).to eq 'Fall 2018'
         expect(terms[2].to_english).to eq 'Summer 2018'
@@ -342,16 +192,6 @@ describe Berkeley::Terms do
     end
     it 'reports an error' do
       expect(subject[:statusCode]).to eq 503
-    end
-  end
-
-  context 'Hub Term API feature flag disabled' do
-    before { allow(Settings.features).to receive(:hub_term_api).and_return false }
-    let(:fake_now) {DateTime.parse('2016-06-16')}
-    it 'uses only the legacy DB, even for non-legacy semesters' do
-      expect(HubTerm::Proxy).to receive(:new).never
-      expect(subject.next.legacy?).to be_falsey
-      expect(subject.future).to be_nil
     end
   end
 
